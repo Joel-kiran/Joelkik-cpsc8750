1d0ac3765f694102f64bd96fd200d3c609781e20
Joel Kiran Kumar <joelkirankumar@Joels-MBP.vyvebroadband.net>
2022-10-28 15:49:12 -0400

    API Layer

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 
 // create a new server application
+const fetch = require('node-fetch');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -28,6 +29,47 @@ app.get('/', (req, res) => {
         str: cookie['visited'] == null ? "You have never visited" : "It has been " +
             Math.round((Date.now() - new Date(parseInt(cookie["visited"]))) / 1000) + " seconds since your last visit"
     });
+});
+
+app.get("/trivia", async(req, res) => {
+    // fetch the data
+    const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+    // fail if bad response
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
+
+    // interpret the body as json
+    const content = await response.json();
+
+    // fail if db failed
+    if (content.response_code !== 0) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+        return;
+    }
+
+    // respond to the browser
+    correctAnswer = content.results[0]['correct_answer']
+    answers = content.results[0]['incorrect_answers']
+    answers.push(correctAnswer)
+    let shuffledAnswers = answers.sort(function() {
+        return Math.random() - 0.5;
+    });
+    const answerLinks = shuffledAnswers.map(answer => {
+        return `<a style='color:white' href="javascript:alert('${
+          answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+          }')">${answer}</a>`
+    })
+    res.render('trivia', {
+        question: content.results[0]['question'],
+        category: content.results[0]['category'],
+        difficulty: content.results[0]['difficulty'],
+        answers: answerLinks
+    })
 });
 
 // Start listening for network connections
